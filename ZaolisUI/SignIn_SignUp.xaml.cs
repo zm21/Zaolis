73debcb560c1589e87b3f75c86df5df0c360ae16
zm21 <zaietsmikola.21@gmail.com>
2020-10-28 12:06:24 +0200

    SignIn&SignUp UI

---
+++ ZaolisUI/SignIn_SignUp.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZaolisUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void TOSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            LOGIN.Visibility = Visibility.Hidden;
+            SignUP.Visibility = Visibility.Visible;
+        }
+
+        private void TOLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LOGIN.Visibility = Visibility.Visible;
+            SignUP.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonSignUP_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
