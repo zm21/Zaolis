43c560fbdba60cb09d21bcb401441c766f051764
redfizandi@gmail.com <redfizandi@gmail.com>
2020-10-28 19:30:31 +0200

    BLL Done.  Hosted database. Migrations

---
+++ DAL/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.ZaolisModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "DAL.ZaolisModel";
+        }
+
+        protected override void Seed(DAL.ZaolisModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
