33a68b2a42b5b876aa99f50e759f09a4f09fddc1
redfizandi@gmail.com <redfizandi@gmail.com>
2020-10-29 20:17:03 +0200

    WCF

--- BLL/Models/UserDTO.cs
+++ BLL/Models/UserDTO.cs
@@ -11,7 +11,7 @@ namespace BLL.Models
         public int Id { get; set; }
 
         public string Login { get; set; }
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
