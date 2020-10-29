ea84b232a9d9db8e22c8c6758325d4e772398e97
redfizandi@gmail.com <redfizandi@gmail.com>
2020-10-29 16:54:03 +0200

    Migrated.

---
+++ DAL/Migrations/202010291452432_Registration Verification.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RegistrationVerification : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.VerificationCodes", "Code", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.VerificationCodes", "Code", c => c.String());
+        }
+    }
+}
