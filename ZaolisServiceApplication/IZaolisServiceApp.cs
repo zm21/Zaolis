f6ad38261a27bbe17af4e79b8ca4b75205ea083c
zm21 <zaietsmikola.21@gmail.com>
2020-11-12 01:52:43 +0200

    App

---
+++ ZaolisServiceApplication/IZaolisServiceApp.cs
@@ -0,0 +1,110 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ZaolisServiceApplication
+{
+    [ServiceContract(CallbackContract = typeof(IZaolisCallback))]
+    public interface IZaolisService
+    {
+        [OperationContract]
+        UserDTO Connect(string login, string passwordHash);
+        [OperationContract]
+        void Disconnect(UserDTO user);
+        [OperationContract]
+        void AddUser(UserDTO user);
+        [OperationContract]
+        void AddAvatar(AvatarDTO newAvatar);
+        [OperationContract]
+        void RegisterUser(string email);
+        [OperationContract]
+        bool IsExistsUserByLoginPassword(string login, string password);
+        [OperationContract]
+        bool IsExistsUserByEmail(string email);
+        [OperationContract]
+        UserDTO GetUserByLogin(string login);
+        [OperationContract]
+        IEnumerable<UserDTO> GetAllUsers();
+        [OperationContract]
+        UserDTO GetUserByEmail(string email);
+        [OperationContract]
+        int GetCodeFromEmail(string email);
+        [OperationContract]
+        int GetVerificationCodeFromEmail(string email);
+        [OperationContract]
+        void ForgetPassword(UserDTO user);
+        [OperationContract]
+        void EditUsersPassword(UserDTO user, string pass);
+        [OperationContract]
+        bool Request();
+        [OperationContract]
+        void SendMessage(MessageDTO message);
+        [OperationContract]
+        IEnumerable<UserDTO> GetContacts(UserDTO user);
+        [OperationContract]
+        void AddContact(UserDTO add_to, UserDTO newContact);
+        [OperationContract]
+        UserDTO GetUserByName(string name);
+        [OperationContract]
+        void EditUsersName(UserDTO user, string name);
+        [OperationContract]
+        void EditUsersBio(UserDTO user, string bio);
+        [OperationContract]
+        ChatDTO GetChat(UserDTO user, UserDTO contact);
+        [OperationContract]
+        IEnumerable<ChatDTO> GetUserChats(UserDTO user);
+        [OperationContract]
+        IEnumerable<UserDTO> GetUsersByChat(ChatDTO chat);
+        [OperationContract]
+        UserDTO UpdateUserInfo(UserDTO user);
+        [OperationContract]
+        AvatarDTO GetAvatar(UserDTO user);
+        [OperationContract]
+        UserDTO ConnectByUser(UserDTO user);
+        [OperationContract]
+        IEnumerable<MessageDTO> GetMessagesByChat(ChatDTO chat);
+        [OperationContract]
+        ChatDTO GetChatById(int Id);
+        [OperationContract]
+        void RemoveFriendAndChat(UserDTO deletefrom, UserDTO deletewhom, ChatDTO chat);
+    }
+
+    public interface IZaolisCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void RecieveMessage(MessageDTO message);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class TransferFileInto
+    {
+        [DataMember]
+        public string FileName { get; set; }
+        [DataMember]
+        public string Extention { get; set; }
+        [DataMember]
+        public byte[] data;
+        //bool boolValue = true;
+        //string stringValue = "Hello ";
+
+        //[DataMember]
+        //public bool BoolValue
+        //{
+        //    get { return boolValue; }
+        //    set { boolValue = value; }
+        //}
+
+        //[DataMember]
+        //public string StringValue
+        //{
+        //    get { return stringValue; }
+        //    set { stringValue = value; }
+        //}
+    }
+}
---
+++ ZaolisServiceApplication/ZaolisServiceApp.svc.cs
@@ -0,0 +1,184 @@
+﻿using BLL;
+using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ZaolisServiceApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class ZaolisService : IZaolisService
+    {
+        BLLClass bll = new BLLClass();
+        Dictionary<UserDTO, IZaolisCallback> activeUsers = new Dictionary<UserDTO, IZaolisCallback>();
+
+        public void AddAvatar(AvatarDTO newAvatar)
+        {
+            bll.AddAvatar(newAvatar);
+        }
+
+        public void RegisterUser(string email)
+        {
+            bll.SendRegistrationCode(email);
+        }
+
+        public int GetCodeFromEmail(string email)
+        {
+            return bll.GetRegistrationCode(email).Code;
+        }
+
+        public UserDTO Connect(string login, string password)
+        {
+            var res = bll.GetUserByLoginAndPassword(login, password);
+            if (res != null)
+            {
+                bll.ChangeStatus(res, true);
+                activeUsers.Add(res, OperationContext.Current.GetCallbackChannel<IZaolisCallback>());
+            }
+            return res;
+        }
+
+        public UserDTO ConnectByUser(UserDTO user)
+        {
+            if (user != null)
+            {
+                bll.ChangeStatus(user, true);
+                activeUsers.Add(user, OperationContext.Current.GetCallbackChannel<IZaolisCallback>());
+            }
+            return user;
+        }
+
+        public void Disconnect(UserDTO user)
+        {
+            if (user != null)
+            {
+                bll.ChangeStatus(bll.GetUserByLogin(user.Login), false);
+                activeUsers.Remove(user);
+            }
+        }
+
+        public IEnumerable<UserDTO> GetAllUsers()
+        {
+            return bll.GetAllUsers();
+        }
+
+        public UserDTO GetUserByLogin(string login)
+        {
+            return bll.GetUserByLogin(login);
+        }
+
+        public bool IsExistsUserByEmail(string email)
+        {
+            return bll.IsExistsUserByEmail(email);
+        }
+
+        public bool IsExistsUserByLoginPassword(string login, string password)
+        {
+            return bll.IsExistsUserByLoginPassword(login, password);
+        }
+        public UserDTO GetUserByEmail(string email)
+        {
+            return bll.GetUserByEmail(email);
+        }
+
+        public IEnumerable<UserDTO> GetContacts(UserDTO user)
+        {
+            return bll.GetContacts(user);
+        }
+
+        public int GetVerificationCodeFromEmail(string email)
+        {
+            return bll.GetVerificationCode(email).Code;
+        }
+        public void EditUsersPassword(UserDTO user, string pass)
+        {
+            bll.EditUsersPassword(user, pass);
+        }
+
+        public void ForgetPassword(UserDTO user)
+        {
+            bll.SendForgetPassCode(user);
+        }
+        public void AddUser(UserDTO user)
+        {
+            bll.AddUser(user);
+        }
+        public bool Request()
+        {
+            return bll.GetVerificationCode("") != null;
+        }
+
+        public void SendMessage(MessageDTO message)
+        {
+            if (activeUsers.Where(u => u.Key.Id == message.UserId) != null)
+            {
+                bll.AddMessage(message);
+                OperationContext.Current.GetCallbackChannel<IZaolisCallback>().RecieveMessage(message);
+            }
+        }
+        public void AddContact(UserDTO add_to, UserDTO newContact)
+        {
+            bll.AddContact(add_to, newContact);
+        }
+        public UserDTO GetUserByName(string name)
+        {
+            return bll.GetUserByName(name);
+        }
+
+        public void EditUsersName(UserDTO user, string name)
+        {
+            bll.EditUsersName(user, name);
+        }
+
+        public void EditUsersBio(UserDTO user, string bio)
+        {
+            bll.EditUsersBio(user, bio);
+        }
+
+        public ChatDTO GetChat(UserDTO user, UserDTO contact)
+        {
+            return bll.GetChat(user, contact);
+        }
+
+        public IEnumerable<ChatDTO> GetUserChats(UserDTO user)
+        {
+            return bll.GetUserChats(user);
+        }
+
+        public IEnumerable<UserDTO> GetUsersByChat(ChatDTO chat)
+        {
+            return bll.GetUsersByChat(chat);
+        }
+
+        public AvatarDTO GetAvatar(UserDTO user)
+        {
+            return bll.GetAvatar(user);
+        }
+
+        public IEnumerable<MessageDTO> GetMessagesByChat(ChatDTO chat)
+        {
+            return bll.GetMessagesByChat(chat);
+        }
+
+        public ChatDTO GetChatById(int Id)
+        {
+            return bll.GetChatById(Id);
+        }
+
+        public void RemoveFriendAndChat(UserDTO deletefrom, UserDTO deletewhom, ChatDTO chat)
+        {
+            bll.RemoveFriendAndChat(deletefrom, deletewhom, chat);
+        }
+
+        public UserDTO UpdateUserInfo(UserDTO user)
+        {
+            var res = bll.GetUserByLogin(user.Login);
+            return res;
+        }
+    }
+}
